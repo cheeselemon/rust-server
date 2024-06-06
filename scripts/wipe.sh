fc7f0072666a2ec71dfdf38d7264ee32efc8d468
cheeselemon <cheeselemon.yarn@gmail.com>
2024-06-06 23:16:48 +0900

    Add cron job to wipe Rust server every Friday at 3am KST

--- scripts/wipe.sh
+++ scripts/wipe.sh
@@ -7,7 +7,7 @@
 sudo docker compose down
 
 # replace RUST_SERVER_SEED={todays_date} in .env
-sed -i "s/RUST_SERVER_SEED=.*/RUST_SERVER_SEED=$(date +%Y%m%d)/" .env
+sed -i "s/RUST_SERVER_SEED=.*/RUST_SERVER_SEED=$(date +%Y%m%d)/" ../.env
 
 # up
 sudo docker compose up -d
